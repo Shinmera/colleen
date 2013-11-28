2e5884db50b30a0040f70750f51dc05ce99f8c4a
Shinmera <shinmera@tymoon.eu>
2013-11-28 19:05:58 +0100

    Continued work on the dramatica moudle.

--- modules/dramatica.lisp
+++ modules/dramatica.lisp
@@ -11,16 +11,29 @@
 (in-package :org.tymoonnext.colleen.mod.dramatica)
 
 (ql:quickload :cl-wiki)
-(setf wiki:*wiki-api* "http://encyclopediadramatica.se/api.php")
+(ql:quickload :xencl)
 
 (define-module dramatica ()
     ((%log-running :initarg :log-running :initform NIL :accessor log-running)
      (%log-loop :initarg :log-loop :initform NIL :accessor log-loop))
   (:documentation "Module related to Encyclopedia Dramatica activities."))
 
-(define-group ed :documentation "Interact or change ED settings.")
+(defmethod start ((dramatica dramatica))
+  (setf wiki:*wiki-api* (config-tree :dramatica :wiki :api))
+  (when (> (length (config-tree :dramatica :wiki :pass)) 0)
+    (wiki:login (config-tree :dramatica :wiki :user)
+                (config-tree :dramatica :wiki :pass)))
+  (when (> (length (config-tree :dramatica :forum :pass)) 0)
+    (xencl:initiate (config-tree :dramatica :forum :url)
+                    (config-tree :dramatica :forum :user)
+                    (config-tree :dramatica :forum :pass))))
+
+(defmethod stop ((dramatica dramatica))
+  (setf (config-tree :dramatica :wiki :api) wiki:*wiki-api*)
+  (setf (config-tree :dramatica :forum :url) xencl:*index*))
 
 ;;;;;;;;;;;; RECENT CHANGES
+(define-group ed :documentation "Interact or change ED settings.")
 
 (define-command (ed recent-changes) (&optional start-or-stop) (:authorization T :documentation "Start or stop the recent-changes polling.")
   (cond
@@ -103,8 +116,94 @@
              (wiki:page-append page "{{welcome}} ~~~~")
              (v:info :dramatica.handle-log "<~a> Welcome page created." page)))))))
 
-;;;;;;;;;;;; OTHER COMMANDS
+;;;;;;;;;;;; WIKI COMMANDS
 
-(define-command (wiki login) (username password) (:authorization T :documentation "Log in to the wiki.")
-  )
+(define-group wiki :documentation "Commands for the Encyclopedia Dramatica Wiki.")
 
+(define-command (wiki api-page) (&optional new-url) (:authorization T :documentation "Change or view the used MediaWiki API URL.")
+  (when new-url
+    (setf wiki:*wiki-api* new-url))
+  (respond event "Used API: ~a" wiki:*wiki-api*))
+
+(define-command (wiki login) (&optional username password) (:authorization T :documentation "Log in to the wiki.")
+  (handler-case
+      (progn
+        (wiki:login (or username (config-tree :dramatica :wiki :user)) 
+                    (or password (config-tree :dramatica :wiki :pass)))
+        (respond event "Login successful."))
+    (error (err)
+      (respond event "Error: ~a" err))))
+
+(defmacro define-wiki-command (name (&rest args) (&rest options) &body body)
+  `(define-command (wiki ,name) ,args ,options
+     (handler-case
+         (progn ,@body)
+       (wiki:wiki-error (err)
+         (respond event "Wiki error: ~a (E~a)" (wiki:info err) (wiki:code err))))))
+
+(define-wiki-command token () (:authorization T :documentation "Retrieve the edit-token.")
+  (respond event "Token: ~a" (wiki:token)))
+
+(define-command page (page) (:documentation "Retrieve the contents of a page.")
+  (respond event (wiki:page-get page)))
+
+(define-command prepend (page &rest content) (:authorization T :documentation "Prepend content to a page.")
+  (wiki:page-prepend page (format NIL "~{~a~^ ~}" content))
+  (respond event "Page edited."))
+
+(define-command append (page &rest content) (:authorization T :documentation "Append content to a page.")
+  (wiki:page-append page (format NIL "~{~a~^ ~}" content))
+  (respond event "Page edited."))
+
+(define-command create (page &rest content) (:authorization T :documentation "Create a new page.")
+  (wiki:page-create page (format NIL "~{~a~^ ~}" content))
+  (respond event "Page created."))
+
+(define-command edit (page &rest content) (:authorization T :documentation "Edit the content of a page.")
+  (wiki:page-edit page (format NIL "~{~a~^ ~}" content))
+  (respond event "Page edited."))
+
+(define-command delete (page) (:authorization T :documentation "Delete a page.")
+  (wiki:page-delete page)
+  (respond event "Page deleted."))
+
+(define-command protect (page &optional (expiry "never") cascade &rest reason) (:authorization T :documentation "Protect a page from edits.")
+  (setf cascade (string-equal cascade "yes")
+        reason (format NIL "~{~a~^ ~}" reason))
+  (wiki:page-protect page :expiry expiry :cascade cascade :reason reason)
+  (respond event "Page protected until ~a (~a)~@[~* cascading the protect~]." expiry reason cascade))
+
+(define-command rollback (page &rest edit-summary) (:authorization T :documentation "Roll a page back to the previous change.")
+  (wiki:page-rollback page :summary (format NIL "~{~a~^ ~}" edit-summary))
+  (respond event "Page rollback performed."))
+
+(define-command block (user &optional (expiry "never") &rest reason) (:authorization T :documentation "Block a user from the wiki.")
+  (setf reason (format NIL "~{~a~^ ~}" reason))
+  (wiki:user-block user :expiry expiry :reason reason)
+  (respond event "User blocked until ~a (~a)." expiry reason))
+
+;;;;;;;;;;;; FORUM COMMANDS
+
+(define-group edf :documentation "Commands for the Encyclopedia Dramatica Forums.")
+
+(define-command (edf url) (&optional url) (:authorization T :documentation "View or set the forum URL.")
+  (when url 
+    (setf xencl:*index* url))
+  (respond event "Used URL: ~a" xencl:*index*))
+
+(defmacro define-edf-command (name (&rest args) (&rest options) &body body)
+  `(define-command (edf ,name) ,args ,options
+     (handler-case
+         (progn ,@body)
+       (xencl:forum-error (err)
+         (respond event "Forum error: ~a (E~a) ~@[on page ~a~]" (xencl:info err) (xencl:code err) (xencl:page err))))))
+
+(define-edf-command login (&optional username password) (:authorization T :documentation "Log in to the forum.")
+  (xencl:login (make-instance 'xencl:user
+                              :title (or username (config-tree :dramatica :forum :user))
+                              :pass (or password (config-tree :dramatica :forum :pass))))
+  (respond event "Login successful."))
+
+(define-edf-command shoutbox-post (&rest message) (:authorization T :documentation "Posted a message to the shoutbox.")
+  (xencl:post (make-instance 'xencl:shoutbox) (format NIL "~{~a~^ ~}" message))
+  (respond event "Message posted."))
