55dcf56f8c89e1650846160e7580c63a89e93a02
Shinmera <shinmera@tymoon.eu>
2013-12-18 11:20:09 +0100

    Added automatic GHOST and nick switch.

--- modules/core.lisp
+++ modules/core.lisp
@@ -9,7 +9,7 @@
 (define-module core () () (:documentation "Colleen core module, handling a few standard events."))
 (start (get-module :core))
 
-(define-handler (events::welcome-event event) ()
+(define-handler (events:welcome-event event) ()
   (v:info (name (server event)) "Got welcome, joining channels.")
   (let ((nickservpw (server-config (name (server event)) :nickservpw)))
     (when nickservpw
@@ -20,17 +20,30 @@
      do (irc:join chan)
        (irc:privmsg chan (standard-message :join))))
 
-(define-handler (events::pong-event event) ()
+(define-handler (events:pong-event event) ()
   (setf (last-ping (server event)) (get-universal-time)))
 
-(define-handler (events::ping-event event) ()
+(define-handler (events:ping-event event) ()
   (setf (last-ping (server event)) (get-universal-time))
-  (irc:pong (events::server1 event)))
+  (irc:pong (events:server1 event)))
 
-(define-handler (events::nick-event event) ()
-  (when (string-equal (events::old-nick event) (nick (server event)))
+(define-handler (events:nick-event event) ()
+  (when (string-equal (events:old-nick event) (nick (server event)))
     (v:debug (name (server event)) "Changing nick of server to ~a due to NICK event." (nick event))
-    (setf (nick server event) (nick event))))
+    (setf (nick (server event)) (nick event))))
 
-(define-handler (events::nickname-in-use-event event) ()
-  (irc:nick (format NIL "~a_" (server-config (name (server event)) :nick))))
+(define-handler (events:nickname-in-use-event event) ()
+  (let ((true-nick (server-config (name (server event)) :nick))
+        (nick-pass (server-config (name (server event)) :nickservpw)))
+    (when (string-equal (nick event) true-nick)
+      (irc:nick (format NIL "~a_" true-nick))
+      (setf (nick (server event)) (format NIL "~a_" true-nick))
+      (when nick-pass
+        (sleep 1)
+        (irc:privmsg "NickServ" (format NIL "GHOST ~a ~a" true-nick nick-pass))))))
+
+(define-handler (events:quit-event event) ()
+  (let ((true-nick (server-config (name (server event)) :nick)))
+    (when (and (string-equal (nick (server event)) (format NIL "~a_" true-nick))
+               (string-equal (nick event) true-nick))
+      (irc:nick true-nick))))
