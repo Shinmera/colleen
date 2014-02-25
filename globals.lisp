903da0a3144ca0d4efda7c42b2798b7a13cf56fb
Shinmera <shinmera@tymoon.eu>
2014-02-25 10:52:56 +0100

    Added sbcl specific read for synchronized hash tables

--- globals.lisp
+++ globals.lisp
@@ -6,9 +6,9 @@
 
 (in-package :org.tymoonnext.colleen)
 
-(defvar *servers* (make-hash-table :synchronized T) "Table containing the IRC server instances.")
-(defvar *bot-modules* (make-hash-table :synchronized T) "Global module table consisting of name->instance pairs.")
-(defvar *event-map* (make-hash-table :synchronized T :test 'equal) "Global event map for event codes to event classes.")
+(defvar *servers* (make-hash-table #+sbcl :synchronized #+sbcl T) "Table containing the IRC server instances.")
+(defvar *bot-modules* (make-hash-table #+sbcl :synchronized #+sbcl T) "Global module table consisting of name->instance pairs.")
+(defvar *event-map* (make-hash-table :test 'equal #+sbcl :synchronized #+sbcl T) "Global event map for event codes to event classes.")
 (defvar *conf-file* NIL "Pathname pointing to the config file being used.")
 (defvar *conf* NIL "ALIST structure of the global configuration.")
 (defvar *debugger* NIL "Boolean indicating whether to invoke the debugger on an unhandled condition.")
