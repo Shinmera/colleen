b43aa54ed309c6742c46fe59107fed748414e6c8
Shinmera <shinmera@tymoon.eu>
2014-12-13 13:59:27 +0100

    Added IRC NOTIFY command

--- modules/essentials.lisp
+++ modules/essentials.lisp
@@ -256,6 +256,9 @@
 (define-command (irc broadcast) (&rest message) (:authorization T :documentation "Broadcast a message to all channels and servers.")
   (irc:broadcast (format NIL "~{~a~^ ~}" message)))
 
+(define-command (irc notify) (target &rest message) (:authorization T :documentation "Notify all users in the target channel with a message.")
+  (irc:privmsg target (format NIL "~{~a: ~}~{~a~^ ~}" (users (channel event) (server event)) message)))
+
 (define-irc-server-command nick (new-nick &optional server) (:documentation "Change the bot's nick.")
   (irc:nick new-nick :server serv))
 
