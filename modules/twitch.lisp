6121d63718a24870f7dae7ec8da2331e3f3373de
Shinmera <shinmera@tymoon.eu>
2016-02-10 23:51:26 +0100

    Fixed ban-emotes to actually do something, added status logs

--- modules/twitch.lisp
+++ modules/twitch.lisp
@@ -36,85 +36,105 @@
 
 (define-twitch-command color (color) ()
   (send ".color ~a" color)
+  (v:info :twitch "[~a] Changing color to ~a" channel color)
   (respond event "Color changed to ~a." color))
 
 (define-twitch-command ignore (user) ()
   (send ".ignore ~a" user)
-  (respond event "User ~s ignored." user))
+  (v:info :twitch "[~a] Ignoring ~a" channel user)
+  (respond event "User ~a ignored." user))
 
 (define-twitch-command unignore (user) ()
   (send ".unignore ~a" user)
-  (respond event "User ~s unignored." user))
+  (v:info :twitch "[~a] Unignoring ~a" channel user)
+  (respond event "User ~a unignored." user))
 
 (define-twitch-command me (text) ()
   (send ".me ~a" text))
 
 (define-twitch-command timeout (user &optional time) ()
   (send ".timeout ~a~@[ ~a~]" user time)
+  (v:info :twitch "[~a] Timing out ~a~@[ for ~a~]." channel user time)
   (respond event "User ~s has been timed out~@[ for ~a seconds~]." user time))
 
 (define-twitch-command ban (user) ()
   (send ".ban ~a" user)
+  (v:info :twitch "[~a] Banning ~a" channel user)
   (respond event "User ~s has been banned." user))
 
 (define-twitch-command unban (user) ()
   (send ".unban ~a" user)
+  (v:info :twitch "[~a] Unbanning ~a" channel user)
   (respond event "User ~s has been unbanned." user))
 
 (define-twitch-command slow (&optional seconds) ()
   (cond (seconds
          (send ".slow ~a" seconds)
+         (v:info :twitch "[~a] Enabling slow mode." channel)
          (respond event "Slow mode has been activated~@[ and set to ~a seconds~]." seconds))
         (T
          (send ".slowoff")
+         (v:info :twitch "[~a] Disabling slow mode." channel)
          (respond event "Slow mode has been deactivated."))))
 
 (define-twitch-command subscribers-only (&optional (on "on")) ()
   (cond ((string-equal on "on")
          (send ".subscribers")
+         (v:info :twitch "[~a] Enabling subscribers-only." channel)
          (respond event "Subscribers-only mode has been activated."))
         (T
          (send ".subscribersoff")
+         (v:info :twitch "[~a] Disabling subscribers-only." channel)
          (respond event "Subscribers-only mode has been deactivated."))))
 
 (define-twitch-command emote-only (&optional (on "on")) ()
   (cond ((string-equal on "on")
          (send ".emoteonly")
+         (v:info :twitch "[~a] Enabling emote-only." channel)
          (respond event "Emote-only mode has been activated."))
         (T
          (send ".emoteoff")
+         (v:info :twitch "[~a] Disabling emote-only." channel)
          (respond event "Emote-only mode has been deactivated."))))
 
 (define-twitch-command clear () ()
   (send ".clear")
+  (v:info :twitch "[~a] Clearing." channel)
   (respond event "Channel cleared."))
 
 (define-twitch-command r9k (&optional (on "on")) ()
   (cond ((string-equal on "on")
          (send ".r9kbeta")
+         (v:info :twitch "[~a] Activating R9K." channel)
          (respond event "R9K mode has been activated."))
         (T
          (send ".r9kbetaoff")
+         (v:info :twitch "[~a] Deactivating R9K." channel)
          (respond event "R9K mode has been deactivated."))))
 
 (define-twitch-command commercial (&optional seconds) ()
   (send ".commercial~@[ ~a~]" seconds)
+  (v:info :twitch "[~a] Starting commercial." channel)
   (respond event "Running a commercial~@[ for ~a seconds~]." seconds))
 
-(define-twitch-command host (&optional channel) ()
+(define-twitch-command host () ()
   (cond (channel
          (send ".host ~a" channel)
+         (v:info :twitch "[~a] Hosting ~a." channel)
          (respond event "Now hosting ~a" channel))
         (T
          (send ".unhost")
+         (v:info :twitch "[~a] Unhosting." channel)
          (respond event "Now no longer hosting."))))
 
 (define-twitch-command mod (user) ()
   (send ".mod ~a" user)
+  (v:info :twitch "[~a] Modding ~a." channel)
   (respond event "User ~a has been modded." user))
 
 (define-twitch-command unmod (user) ()
   (send ".unmod ~a" user)
+  (v:info :twitch "[~a] Demodding ~a." channel)
   (respond event "User ~a has been unmodded." user))
 
 (defvar *emotes* ())
@@ -128,6 +148,7 @@
     (apply #'jsown:parse data keywords)))
 
 (defun load-emotes ()
+  (v:info :twitch "Loading emotes.")
   ;; We load this file instead as it is more lightweight for what we want.
   (let ((raw (json-request "https://twitchemotes.com/api_cache/v2/images.json" "images")))
     (setf *emotes* (mapcar (lambda (obj) (cdr (assoc "code" (cddr obj) :test #'string=)))
@@ -137,12 +158,15 @@
   (load-emotes)
   (respond event "Emotes reloaded."))
 
-(define-twitch-command ban-emotes (&optional (mode "600") (channel (channel event))) ()
+(define-twitch-command ban-emotes (&optional (mode "600")) ()
   (let ((mode (cond ((string-equal mode "none") NIL)
                     ((string-equal mode "scold") :scold)
                     ((string-equal mode "infinity") :infinity)
                     ((parse-integer mode :junk-allowed T)
                      (parse-integer mode :junk-allowed T)))))
+    (setf (gethash channel (or (uc:config-tree :emotes-banned)
+                               (setf (uc:config-tree :emotes-banned) (make-hash-table :test 'equalp)))) mode)
+    (v:info :twitch "[~a] Setting emote ban mode to ~a" channel mode)
     (if mode
         (respond event "Emotes are now banned.")
         (respond event "Emotes are now unbanned."))))
